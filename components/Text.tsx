8f9d032f1781743293bd28dc4d45eaa28b971001
Parag <paragmmbeta@gmail.com>
2026-01-07 17:17:00 +0530

    Install shadcn and mate soner visible image

---
+++ components/Text.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react';
+import { Button } from './ui/button';
+import { toast } from 'sonner';
+
+
+const Text = () => {
+    const clickHandler = (mode: boolean) => {
+        if (mode) {
+            toast.success("This is a success toast!")
+        } else {
+            toast.error("This is an error toast!")
+        }
+    }
+    return (
+        <div>
+            <Button className="bg-red-600" variant="default" onClick={() => clickHandler(false)}>Button</Button>
+            Hellow text
+
+        </div>
+    );
+};
+
+export default Text;
